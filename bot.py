938e18e3bca985147a9646f2b1a699bb922fcfcb
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-24 00:30:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
+from pyromod import listen
 from pyrogram import Client, filters
 import logging
 
