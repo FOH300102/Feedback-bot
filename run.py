e089eac16d828196b8855399c149eabfad760c40
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-24 13:53:11 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,28 @@
+# @KEK_Projects
+
+import pyromod.listen
+from pyrogram import Client
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+
+app = Client(
+    "bot",
+    api_id=env.API_ID,
+    api_hash=env.API_HASH,
+    bot_token=env.BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="database"),
+)
+
+if __name__ == "__main__":
+    print("Starting Private Chat Bot...")
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"@{uname} started successfully !")
+    idle()
+    app.stop()
+    print("Bot stopped. Bye !")
